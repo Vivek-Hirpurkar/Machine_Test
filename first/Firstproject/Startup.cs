31a50859e71b912c7f9db6f19062365d532856c1
vivek <vivekhirpurkar1@gmail.com>
2020-05-14 12:15:36 +0530

    new

---
+++ first/Firstproject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Firstproject.Startup))]
+namespace Firstproject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
